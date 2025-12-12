3e8f90998be84da3773e55be2f11d3457454f1cb
Pangpang0255 <naufalazzadhiyaa@gmail.com>
2025-12-13 00:19:57 +0700

    Responsive Layout

--- src/components/Elements/LabeledInput.jsx
+++ src/components/Elements/LabeledInput.jsx
@@ -1,29 +1,17 @@
-import React from 'react'
+import React from "react";
+import Input from "./Input";
 
 function LabeledInput(props) {
-    const { label, type, placeholder, name, id, icon, ...rest } = props;
+  const { label, id, ...rest } = props;
+
   return (
     <>
-    <label htmlFor={id} className="block text-sm mb-2">
-                {label}
-              </label>
-              <div className="relative">
-                <input
-                  type={type}
-                  placeholder={placeholder}
-                  name={name}
-                  className={`py-3 pl-4 text-sm rounded-md w-full bg-special-mainBg border border-gray-03 text-gray-01 focus:border-black focus:outline-none focus:ring-0 ${icon ? 'pr-12' : ''}`}
-                  id={id}
-                  {...rest}
-                />
-                {icon && (
-                  <div className="absolute right-3 top-1/2 transform -translate-y-1/2">
-                    {icon}
-                  </div>
-                )}
-              </div>
+      <label htmlFor={id} className="block text-sm mb-2">
+        {label}
+      </label>
+      <Input id={id} {...rest} />
     </>
-  )
+  );
 }
 
-export default LabeledInput
+export default LabeledInput;
